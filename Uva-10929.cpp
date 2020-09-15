667997ffc42291c404c6aa641d9b75442bdc9fa9
Jian <l059069045@gmail.com>
2020-09-15 21:22:18 +0800

    all

---
+++ Uva-10929.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef DEBUG
+#define cout fout
+#define cin fin
+ifstream fin("in.txt");
+ofstream fout("out.txt");
+#endif
+
+int main(){
+    
+    return 0;
+}
