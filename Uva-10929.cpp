acc4edd4c4eac1a3366d698882b8a9459c90f9f4
Jian <l059069045@gmail.com>
2020-09-16 08:18:37 +0800

    best

--- Uva-10929.cpp
+++ Uva-10929.cpp
@@ -9,7 +9,23 @@ ifstream fin("in.txt");
 ofstream fout("out.txt");
 #endif
 
-int main(){
-    
+int main() {
+    string num;
+    while (cin >> num,num!="0") {
+        int odd = 0, even = 0;
+        bool k=0;
+        for(int i=0;i<num.size();i++) {
+            if(i%2==0)
+                odd=odd+(num[i]-'0');
+            else
+                even=even+(num[i]-'0');
+        }
+        int ans;
+        ans = abs(odd - even);
+        if (ans % 11)
+            cout << num << " is not a multiple of 11." << endl;
+        else
+            cout << num << " is a multiple of 11." << endl;
+    }
     return 0;
 }
